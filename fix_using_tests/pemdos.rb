6274f566bb9ae46fa5275f6794893dc98437b662
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-04 20:34:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -6,16 +6,8 @@ require "pry"
 
 def snake_it_up(string)
   if string[0] == "s"
- #binding.pry
-   ten_s= 10 * "s" 
-   
+   ten_s = ssssssssss  
    new_word = ten_s + string
-    #binding.pry 
-    #10.times do print "s" + string
-    #end
-    
-  
-  else
-    new_word
+   return new_word
   end
 end
